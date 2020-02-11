ad6ac1e0c5c5a393d22db73a48ffb99f9309569b
Lucas Wilber <lucasjwilber@gmail.com>
2020-02-11 12:12:06 -0800

    added animation for when 'add task' is clicked

--- app/src/main/java/com/example/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -2,6 +2,8 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +15,19 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
     }
+    //thanks to https://developer.android.com/training/animation/reveal-or-hide-view
     public void addTaskButtonClicked(View v) {
-        View submitMessage = findViewById(R.id.submitMessage);
+        final View submitMessage = findViewById(R.id.submitMessage);
         submitMessage.setVisibility(View.VISIBLE);
+        submitMessage.animate()
+                .alpha(0.0f)
+                .setDuration(1000)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        submitMessage.setVisibility(View.INVISIBLE);
+                        submitMessage.setAlpha(1.0f);
+                    }
+                });
     }
 }
