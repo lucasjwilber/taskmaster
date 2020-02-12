68d806f0403338c161e1d96044f3e551f14ea8a3
Lucas Wilber <lucasjwilber@gmail.com>
2020-02-12 11:20:37 -0800

    added settings page and task details page, added method to save username in sharedprefs via settings page, added method to update homepage title with saved username, added fake tasks to homepage

--- app/src/main/java/com/lucasjwilber/taskmaster/MainActivity.java
+++ app/src/main/java/com/lucasjwilber/taskmaster/MainActivity.java
@@ -3,8 +3,11 @@ package com.lucasjwilber.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//
+//        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+//        String username = sharedPref.getString("username", "My ");
+//        TextView titleView = findViewById(R.id.titleMain);
+//        String titleText = username + "Tasksss";
+//        titleView.setText(titleText);
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = prefs.getString("username", "My ");
+        TextView titleView = findViewById(R.id.titleMain);
+
+        String titleText;
+        if (username.lastIndexOf("s") == username.length() - 1) {
+            titleText = username + "' Tasks";
+        } else {
+            titleText = username + "'s Tasks";
+        }
+        titleView.setText(titleText);
+    }
 
     public void goToAddTasksActivity(View v) {
         Intent intent = new Intent(this, AddTaskActivity.class);
@@ -22,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToAllTasksActivity(View v) {
         Intent intent = new Intent(this, AllTasksActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSettingsActivity(View v) {
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/lucasjwilber/taskmaster/TaskDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.lucasjwilber.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
